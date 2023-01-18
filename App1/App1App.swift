9cc1297579ff9f28be846781dea79588a62641e2
Abdurakhmon <abdur@basemap.com>
2023-01-18 21:23:45 +0500

    Initial Commit

---
+++ App1/App1App.swift
@@ -0,0 +1,17 @@
+//
+//  App1App.swift
+//  App1
+//
+//  Created by Workspace on 18/01/23.
+//
+
+import SwiftUI
+
+@main
+struct App1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
